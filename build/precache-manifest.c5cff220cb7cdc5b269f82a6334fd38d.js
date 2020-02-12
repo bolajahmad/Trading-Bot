59c89e2f53feb00e62c62b5558e5ef6f2904eeaa
bolajahmad <56865496+bolajahmad@users.noreply.github.com>
2020-02-12 13:43:04 +0100

    Add files via upload

---
+++ build/precache-manifest.c5cff220cb7cdc5b269f82a6334fd38d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5474ce6872dea36461ab9ec5c23934c4",
+    "url": "/Trading-Bot/index.html"
+  },
+  {
+    "revision": "b4b369c9f72af5b72f23",
+    "url": "/Trading-Bot/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c733efeb03954b109939",
+    "url": "/Trading-Bot/static/css/main.d644fcae.chunk.css"
+  },
+  {
+    "revision": "b4b369c9f72af5b72f23",
+    "url": "/Trading-Bot/static/js/2.be08112c.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/Trading-Bot/static/js/2.be08112c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c733efeb03954b109939",
+    "url": "/Trading-Bot/static/js/main.4aea5ede.chunk.js"
+  },
+  {
+    "revision": "0824be1fec5feaccb7a1",
+    "url": "/Trading-Bot/static/js/runtime-main.5ad1bdb0.js"
+  },
+  {
+    "revision": "cecfa06fa8b1d54203a4fa912434bb83",
+    "url": "/Trading-Bot/static/media/Augur.cecfa06f.png"
+  },
+  {
+    "revision": "8f1317ba84831e6dfd1c9da76194e66d",
+    "url": "/Trading-Bot/static/media/Bitcoin.8f1317ba.jpg"
+  },
+  {
+    "revision": "dccce16b2ba04822f2818a11b6402fc6",
+    "url": "/Trading-Bot/static/media/Cardano.dccce16b.png"
+  },
+  {
+    "revision": "9238551e012a3dd940e5088ba6af5624",
+    "url": "/Trading-Bot/static/media/Ethereum.9238551e.png"
+  },
+  {
+    "revision": "5133c6fed045bfde9837184dfac270cf",
+    "url": "/Trading-Bot/static/media/Litecoin.5133c6fe.png"
+  },
+  {
+    "revision": "f53936a0fb3f9f0925f9b36bb2043525",
+    "url": "/Trading-Bot/static/media/Qtum.f53936a0.jpg"
+  },
+  {
+    "revision": "061924023339e1d2b01a345d33f61c3c",
+    "url": "/Trading-Bot/static/media/Siacoin.06192402.png"
+  },
+  {
+    "revision": "6ec6dc227d9fbaddc65506d46aa02a78",
+    "url": "/Trading-Bot/static/media/Tron.6ec6dc22.png"
+  }
+]);
